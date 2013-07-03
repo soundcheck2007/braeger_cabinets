ecbeeafab97f72408ac68352bded98c4a695f552
Andrew Ayers <ayers.andy.m@gmail.com>
2013-07-02 19:22:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BraegerCabinets::Application.config.secret_token = 'fe7c56202b086593a4eab8979bcd5b6510aa9f737708cad59d0400c7fd5d69c1e3091393a49a566a67cfa2a027b055bc555ed6ead45c1013a22c707868422a9d'
